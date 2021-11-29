79eeac588e9f47a245769455c1d94c7c9ec1d993
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-29 16:02:08 -0500

    more detailed boot output

--- src_esp32_master/main.ino
+++ src_esp32_master/main.ino
@@ -3,8 +3,8 @@
 
 VGA3Bit vga;
 
-const int PIN_R = 14;
-const int PIN_G = 19;
+const int PIN_R = 25;
+const int PIN_G = 26;
 const int PIN_B = 27;
 const int PIN_VSYNC = 33;
 const int PIN_HSYNC = 32;
@@ -13,7 +13,7 @@ char pin_info[64] = "";
 
 void setup() {
 
-    sprintf(pin_info, "PIN_R=%d PIN_G=%d PIN_B=%d PIN_HSYNC=%d PIN_VSYNC=%d", PIN_R, PIN_G, PIN_B, PIN_HSYNC, PIN_VSYNC);
+    sprintf(pin_info, "PIN_R=%d\nPIN_G=%d\nPIN_B=%d\nPIN_HSYNC=%d\nPIN_VSYNC=%d", PIN_R, PIN_G, PIN_B, PIN_HSYNC, PIN_VSYNC);
 
     vga.init(vga.MODE500x480, PIN_R, PIN_G, PIN_B, PIN_HSYNC, PIN_VSYNC);
     vga.setFont(CodePage437_9x16);
@@ -23,11 +23,24 @@ void setup() {
     vga.println("520KB RAM BUILT IN/0KB EXTERNAL");
     vga.println("80MHZ CLOCK SPEED");
     vga.println("500X400 PIXELS TOTAL");
+    vga.println("");
+
     vga.println(pin_info);
     vga.println("");
+
     vga.println("USING ESP32LIB VGA DRIVER WRITTEN BY BITLUNI");
     vga.println("MAIN PROGRAM WRITTEN BY KATZNBOYZ");
     vga.println("");
+
+    vga.setTextColor(vga.RGB(255, 0, 0), vga.RGB(0, 0, 0));
+    vga.println("RED COLOR TEST");
+    vga.setTextColor(vga.RGB(0, 255, 0), vga.RGB(0, 0, 0));
+    vga.println("GREEN COLOR TEST");
+    vga.setTextColor(vga.RGB(0, 0, 255), vga.RGB(0, 0, 0));
+    vga.println("BLUE COLOR TEST");
+    vga.setTextColor(vga.RGB(255, 255, 255), vga.RGB(0, 0, 0));
+    vga.println("");
+    
     vga.println("DONE BOOTING");
     vga.println("");
 }
