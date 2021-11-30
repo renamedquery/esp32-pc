b8865e42493c80b8ebd8e5cc179ab17505d7a898
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-29 21:30:49 -0500

    made all commands into functions

--- src_esp32_master/main.ino
+++ src_esp32_master/main.ino
@@ -9,21 +9,16 @@
 #define SCREEN_SIZE (SCREEN_WIDTH*SCREEN_HEIGHT)
 #define CLI_LINE_HEIGHT 16
 #define SCREEN_BITDEPTH 3
-#define HELP_MENU_LENGTH 3
 
 VGA3Bit vga;
+
+typedef int (*cli_function)(char[MAX_CLI_INPUT_LENGTH]);
 
 const int PIN_R = 25;
 const int PIN_G = 26;
 const int PIN_B = 27;
 const int PIN_VSYNC = 33;
 const int PIN_HSYNC = 32;
-
-char help_menu[HELP_MENU_LENGTH][MAX_CLI_OUTPUT_LENGTH] = {
-    "fbmem - PRINTS THE AMOUNT OF MEMORY USED BY THE FRAMEBUFFER",
-    "fbinfo - PRINTS THE INFORMATION ABOUT THE FRAMEBUFFERS RESOLUTION AND BIT DEPTH",
-    "hwinfo - PRINTS THE INFORMATION ABOUT WHICH PINS ARE ASSIGNED TO WHAT FUNCTION"
-};
 
 char pin_info[64] = "";
 char line_separator[64] = "---------------------------------------------------------------";
@@ -32,6 +27,119 @@ void scroll_terminal(int lines) {
 
     for (int i = 0; i < lines; i++) vga.scroll(CLI_LINE_HEIGHT, vga.RGB(0, 0, 0));
 }
+
+// ----------------------------------------------------------------------------------------------------------
+// terminal functions below
+// should return 0 upon success
+
+int cli_cmd_fbmem(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    char memused_line1[MAX_CLI_OUTPUT_LENGTH] = "";
+    char memused_line2[MAX_CLI_OUTPUT_LENGTH] = "";
+
+    sprintf(memused_line1, "    FRAMEBUFFER 1: %d BYTES", (SCREEN_SIZE * sizeof(unsigned long)));
+    sprintf(memused_line2, "    FRAMEBUFFER 2: %d BYTES", (SCREEN_SIZE * sizeof(unsigned long)));
+    
+    scroll_terminal(3);
+
+    vga.println("USED FRAMEBUFFER MEMORY:");
+    vga.println(memused_line1);
+    vga.println(memused_line2);
+
+    return 0;
+}
+
+int cli_cmd_fbinfo(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    char fbinfo_line1[MAX_CLI_OUTPUT_LENGTH] = "";
+    char fbinfo_line2[MAX_CLI_OUTPUT_LENGTH] = "";
+    char fbinfo_line3[MAX_CLI_OUTPUT_LENGTH] = "";
+    char fbinfo_line4[MAX_CLI_OUTPUT_LENGTH] = "";
+
+    sprintf(fbinfo_line1, "    SCREEN WIDTH: %d PIXELS", SCREEN_WIDTH);
+    sprintf(fbinfo_line2, "    SCREEN HEIGHT: %d PIXELS", SCREEN_HEIGHT);
+    sprintf(fbinfo_line3, "    SCREEN SIZE: %d PIXELS", SCREEN_SIZE);
+    sprintf(fbinfo_line4, "    SCREEN COLORS: %d BIT", SCREEN_BITDEPTH);
+
+    scroll_terminal(5);
+
+    vga.println("FRAMEBUFFER INFORMATION:");
+    vga.println(fbinfo_line1);
+    vga.println(fbinfo_line2);
+    vga.println(fbinfo_line3);
+    vga.println(fbinfo_line4);
+
+    return 0;
+}
+
+int cli_cmd_hwinfo(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    char hwinfo_line1[MAX_CLI_OUTPUT_LENGTH] = "";
+    char hwinfo_line2[MAX_CLI_OUTPUT_LENGTH] = "";
+    char hwinfo_line3[MAX_CLI_OUTPUT_LENGTH] = "";
+    char hwinfo_line4[MAX_CLI_OUTPUT_LENGTH] = "";
+    char hwinfo_line5[MAX_CLI_OUTPUT_LENGTH] = "";
+    
+    sprintf(hwinfo_line1, "    RED COLOR PIN: %d", PIN_R);
+    sprintf(hwinfo_line2, "    GREEN COLOR PIN: %d", PIN_G);
+    sprintf(hwinfo_line3, "    BLUE COLOR PIN: %d", PIN_B);
+    sprintf(hwinfo_line4, "    VERTICAL SYNC PIN: %d", PIN_VSYNC);
+    sprintf(hwinfo_line5, "    HORIZONTAL SYNC PIN: %d", PIN_HSYNC);
+
+    scroll_terminal(6);
+
+    vga.println("HARDWARE INFORMATION");
+    vga.println(hwinfo_line1);
+    vga.println(hwinfo_line2);
+    vga.println(hwinfo_line3);
+    vga.println(hwinfo_line4);
+    vga.println(hwinfo_line5);
+
+    return 0;
+}
+
+int cli_cmd_help(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    scroll_terminal(5);
+    vga.println("fbmem - PRINTS THE AMOUNT OF MEMORY USED BY THE FRAMEBUFFER");
+    vga.println("fbinfo - PRINTS THE INFORMATION ABOUT THE FRAMEBUFFERS");
+    vga.println("         RESOLUTION AND BIT DEPTH");
+    vga.println("hwinfo - PRINTS THE INFORMATION ABOUT WHICH PINS ARE ASSIGNED");
+    vga.println("         TO WHAT FUNCTION");
+
+    return 0;
+}
+
+int cli_cmd_nop(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    // do nothing
+    // no operation -> nop
+    // this can be used to write comments
+    // ex: `nop this is a comment`
+
+    return 0;
+}
+
+void cli_output(cli_function function, char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    int return_value = function(full_command);
+
+    char return_value_char[MAX_CLI_OUTPUT_LENGTH_PER_LINE] = "";
+
+    sprintf(return_value_char, "%d", return_value);
+
+    scroll_terminal(1);
+    vga.println(return_value_char);
+}
+
+void cli_nocmd() {
+
+    scroll_terminal(1);
+    vga.println("UNKNOWN COMMAND");
+}
+
+// ----------------------------------------------------------------------------------------------------------
+// arduino functions below
 
 void setup() {
 
@@ -93,86 +201,11 @@ void loop() {
         vga.print("\n");
         scroll_terminal(1);
 
-        if (serial_string.substring(0, 5).equals("fbmem")) {
-
-            char memused_line1[MAX_CLI_OUTPUT_LENGTH] = "";
-            char memused_line2[MAX_CLI_OUTPUT_LENGTH] = "";
-
-            sprintf(memused_line1, "    FRAMEBUFFER 1: %d BYTES", (SCREEN_SIZE * sizeof(unsigned long)));
-            sprintf(memused_line2, "    FRAMEBUFFER 2: %d BYTES", (SCREEN_SIZE * sizeof(unsigned long)));
-            
-            scroll_terminal(3);
-
-            vga.println("USED FRAMEBUFFER MEMORY:");
-            vga.println(memused_line1);
-            vga.println(memused_line2);
-        
-        } else if (serial_string.substring(0, 6).equals("fbinfo")) {
-
-            char fbinfo_line1[MAX_CLI_OUTPUT_LENGTH] = "";
-            char fbinfo_line2[MAX_CLI_OUTPUT_LENGTH] = "";
-            char fbinfo_line3[MAX_CLI_OUTPUT_LENGTH] = "";
-            char fbinfo_line4[MAX_CLI_OUTPUT_LENGTH] = "";
-
-            sprintf(fbinfo_line1, "    SCREEN WIDTH: %d PIXELS", SCREEN_WIDTH);
-            sprintf(fbinfo_line2, "    SCREEN HEIGHT: %d PIXELS", SCREEN_HEIGHT);
-            sprintf(fbinfo_line3, "    SCREEN SIZE: %d PIXELS", SCREEN_SIZE);
-            sprintf(fbinfo_line4, "    SCREEN COLORS: %d BIT", SCREEN_BITDEPTH);
-
-            scroll_terminal(5);
-
-            vga.println("FRAMEBUFFER INFORMATION:");
-            vga.println(fbinfo_line1);
-            vga.println(fbinfo_line2);
-            vga.println(fbinfo_line3);
-            vga.println(fbinfo_line4);
-
-        } else if (serial_string.substring(0, 6).equals("hwinfo")) {
-
-            char hwinfo_line1[MAX_CLI_OUTPUT_LENGTH] = "";
-            char hwinfo_line2[MAX_CLI_OUTPUT_LENGTH] = "";
-            char hwinfo_line3[MAX_CLI_OUTPUT_LENGTH] = "";
-            char hwinfo_line4[MAX_CLI_OUTPUT_LENGTH] = "";
-            char hwinfo_line5[MAX_CLI_OUTPUT_LENGTH] = "";
-            
-            sprintf(hwinfo_line1, "    RED COLOR PIN: %d", PIN_R);
-            sprintf(hwinfo_line2, "    GREEN COLOR PIN: %d", PIN_G);
-            sprintf(hwinfo_line3, "    BLUE COLOR PIN: %d", PIN_B);
-            sprintf(hwinfo_line4, "    VERTICAL SYNC PIN: %d", PIN_VSYNC);
-            sprintf(hwinfo_line5, "    HORIZONTAL SYNC PIN: %d", PIN_HSYNC);
-
-            scroll_terminal(6);
-
-            vga.println("HARDWARE INFORMATION");
-            vga.println(hwinfo_line1);
-            vga.println(hwinfo_line2);
-            vga.println(hwinfo_line3);
-            vga.println(hwinfo_line4);
-            vga.println(hwinfo_line5);
-
-        } else if (serial_string.substring(0, 4).equals("help")) {
-
-            for (int i = 0; i < HELP_MENU_LENGTH; i++) {
-
-                String help_menu_string = help_menu[i];
-
-                for (int j = 0; j < MAX_CLI_OUTPUT_LENGTH; j += MAX_CLI_OUTPUT_LENGTH_PER_LINE) {
-
-                    if (!(help_menu_string.substring(j, j + MAX_CLI_OUTPUT_LENGTH_PER_LINE).isEmpty())) {
-
-                        char help_menu_line[MAX_CLI_OUTPUT_LENGTH_PER_LINE];
-                        help_menu_string.substring(j, j + MAX_CLI_OUTPUT_LENGTH_PER_LINE).toCharArray(help_menu_line, MAX_CLI_OUTPUT_LENGTH_PER_LINE + 1);
-
-                        scroll_terminal(1);
-                        vga.println(help_menu_line);
-                    }
-                }
-            }
-
-        } else {
-
-            scroll_terminal(1);
-            vga.println("UNKNOWN COMMAND");
-        }
+        if (serial_string.substring(0, 5).equals("fbmem")) {cli_output(&cli_cmd_fbmem, serial_string_char);}
+        else if (serial_string.substring(0, 6).equals("fbinfo")) {cli_output(&cli_cmd_fbinfo, serial_string_char);} 
+        else if (serial_string.substring(0, 6).equals("hwinfo")) {cli_output(&cli_cmd_hwinfo, serial_string_char);} 
+        else if (serial_string.substring(0, 4).equals("help")) {cli_output(&cli_cmd_help, serial_string_char);} 
+        else if (serial_string.substring(0, 3).equals("nop")) {cli_output(&cli_cmd_nop, serial_string_char);} 
+        else {cli_nocmd();}
     }
 }
