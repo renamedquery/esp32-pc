beace866e26479c70b9f27fe37d0c001d144a26b
katznboyz1 <harrison.lindgren@gmail.com>
2021-12-01 18:51:46 -0500

    added a 'cd' command

--- src_esp32_master/main.ino
+++ src_esp32_master/main.ino
@@ -25,7 +25,7 @@
 #define SD_MISO 19
 #define SD_MOSI 23
 #define SD_SS 5
-#define SD_FILESYSTEM_ROOT "/sd"
+#define SD_FILESYSTEM_ROOT "/sd/"
 #define SLAVE_RESET_PIN 21
 #define PIN_R 27
 #define PIN_G 26
@@ -142,7 +142,7 @@ int cli_cmd_hwinfo(char full_command[MAX_CLI_INPUT_LENGTH]) {
 
 int cli_cmd_help(char full_command[MAX_CLI_INPUT_LENGTH]) {
 
-    scroll_terminal(12);
+    scroll_terminal(14);
     vga.println("fbmem - PRINTS THE AMOUNT OF MEMORY USED BY THE FRAMEBUFFER");
     vga.println("fbinfo - PRINTS THE INFORMATION ABOUT THE FRAMEBUFFERS");
     vga.println("         RESOLUTION AND BIT DEPTH");
@@ -152,9 +152,11 @@ int cli_cmd_help(char full_command[MAX_CLI_INPUT_LENGTH]) {
     vga.println("net <cmd> - EXECUTES AN ACTION ON THE WIFI INTERFACE");
     vga.println("df - PRINTS THE AVAILABLE DISK DRIVES AND THEIR INFORMATION");
     vga.println("sd <cmd> - EXECUTES A COMMAND IN RELATION TO THE SD CARD SLOT");
-    vga.println("mkdir <path> - CREATES A NEW DIRECTORY");
+    vga.println("mkdir <absolute_path> - CREATES A NEW DIRECTORY");
     vga.println("ls - LISTS ALL FILES/FOLDERS IN THE CURRENT DIRECTORY");
-    vga.println("touch <name> - CREATES A FILE IN THE CURRENT DIRECTORY");
+    vga.println("touch <absolute_path> - CREATES A FILE IN THE CURRENT DIRECTORY");
+    vga.println("cd <absolute_path> - SETS THE CURRENT DIRECTORY TO THE");
+    vga.println("                     SPECIFIED PATH");
 
     return 0;
 }
@@ -297,7 +299,7 @@ int cli_cmd_mkdir(char full_command[MAX_CLI_INPUT_LENGTH]) {
 
     for (int i = end_of_first_command; i < MAX_CLI_INPUT_LENGTH; i++) {
 
-        if (command_string.substring(i, i + 1).isEmpty()) {
+        if (command_string[i] == ' ') {
 
             break;
 
@@ -328,6 +330,8 @@ int cli_cmd_mkdir(char full_command[MAX_CLI_INPUT_LENGTH]) {
     known issues:
     - touching a file starting with a period will crash the OS
     - touching a file with the same name as a directory will fail with error code 2
+    
+    if you find more issues, please add to this list or raise an issue on the github repo!
 */
 int cli_cmd_touch(char full_command[MAX_CLI_INPUT_LENGTH]) {
 
@@ -351,11 +355,11 @@ int cli_cmd_touch(char full_command[MAX_CLI_INPUT_LENGTH]) {
 
     if (filename.isEmpty()) return 1;
 
-    if (SD.exists(current_sd_path + "/" + filename)) return 2;
+    if (SD.exists(filename)) return 2;
 
     try {
 
-        File new_file = SD.open(current_sd_path + "/" + filename, "w");
+        File new_file = SD.open(filename, "w");
         new_file.close();
     
     } catch (...) {
@@ -394,6 +398,56 @@ int cli_cmd_ls(char full_command[MAX_CLI_INPUT_LENGTH]) {
     }
 
     directory_contents.close();
+
+    return 0;
+}
+
+int cli_cmd_cd(char full_command[MAX_CLI_INPUT_LENGTH]) {
+
+    String command_string = full_command;
+
+    const int end_of_first_command = 3;
+
+    String directory = "";
+
+    for (int i = end_of_first_command; i < MAX_CLI_INPUT_LENGTH; i++) {
+
+        if (command_string[i] == ' ') {
+
+            break;
+
+        } else {
+
+            directory += command_string.substring(i, i + 1);
+        }
+    }
+
+    if (directory.isEmpty()) return 1;
+
+    try {
+
+        // this will fail if the path doesnt exist, thus returning the error code 2
+        File directory_test_listing = SD.open(directory, "r");
+        directory_test_listing.close();
+
+        current_sd_path = directory;
+
+        scroll_terminal(1);
+
+        char directory_move_status[MAX_CLI_OUTPUT_LENGTH_PER_LINE] = "";
+
+        sprintf(directory_move_status, "CD: %s", current_sd_path.c_str());
+
+        vga.println(directory_move_status);
+
+    } catch (...) {
+
+        scroll_terminal(1);
+
+        vga.println("NOT A DIRECTORY");
+
+        return 2;
+    }
 
     return 0;
 }
@@ -591,6 +645,7 @@ void loop() {
         else if (serial_string.substring(0, 2).equals("df")) {cli_output(&cli_cmd_df, serial_string_char, vga);} 
         else if (serial_string.substring(0, 2).equals("sd")) {cli_output(&cli_cmd_sd, serial_string_char, vga);} 
         else if (serial_string.substring(0, 2).equals("ls")) {cli_output(&cli_cmd_ls, serial_string_char, vga);} 
+        else if (serial_string.substring(0, 2).equals("cd")) {cli_output(&cli_cmd_cd, serial_string_char, vga);} 
         else if (serial_string.substring(0, 5).equals("mkdir")) {cli_output(&cli_cmd_mkdir, serial_string_char, vga);} 
         else if (serial_string.substring(0, 5).equals("touch")) {cli_output(&cli_cmd_touch, serial_string_char, vga);} 
         else if (serial_string.substring(0, 4).equals("help")) {cli_output(&cli_cmd_help, serial_string_char, vga);} 
