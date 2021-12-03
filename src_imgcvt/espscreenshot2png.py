bbc9e1b667eb9c77a42aaeff8886b283fe14e1b2
katznboyz1 <harrison.lindgren@gmail.com>
2021-12-02 21:03:01 -0500

    tried to implement rle - it failed - reverting

--- src_imgcvt/espscreenshot2png.py
+++ src_imgcvt/espscreenshot2png.py
@@ -11,20 +11,30 @@ image_size = image_file_contents.split('\n')[0].split(';')
 image_size[0] = int(image_size[0])
 image_size[1] = int(image_size[1])
 
-output_image = numpy.zeros((image_size[1], image_size[0], 3), dtype = numpy.uint8)
+output_image = numpy.zeros((image_size[1], image_size[0], 1), dtype = numpy.uint8)
 
-for y in range(image_size[1]):
+output_image_pixdata = image_file_contents.split('\n')[1].split(';')
 
-    row = image_file_contents.split('\n')[y + 1].split(';')
+x, y = 0, 0
 
-    for x in range(0, image_size[0] // 2):
+for pair in range(len(output_image_pixdata) // 2 // 2):
 
-        r = int(row[x])
-        g = int(row[x])
-        b = int(row[x])
+    pix_value = int(output_image_pixdata[(pair * 2) + 0])
+    pix_run_length = int(output_image_pixdata[(pair * 2) + 1])
 
-        output_image[y][(x * 2) + 0] = [r, g, b]
-        output_image[y][(x * 2) + 1] = [r, g, b]
+    for pix_run in range(pix_run_length):
+
+        if ((x) >= (image_size[0] / 2)):
+
+            x = 0
+            y += 1
+
+        output_image[y][(x * 2) + 0] = pix_value
+        output_image[y][(x * 2) + 1] = pix_value
+
+        x += 1
+    
+    x += 1
 
 image_file.close()
 
